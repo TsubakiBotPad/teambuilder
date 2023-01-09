15e9f6dc2a991113bcf19d7dcfccfd9a254439fd
River <18037011+RheingoldRiver@users.noreply.github.com>
2023-01-09 01:00:30 -0600

    Show badge in unbindable % (#77)
    * Show badge in unbindable %
    
    * use for-in loop

--- src/components/teamStats/unbindable.tsx
+++ src/components/teamStats/unbindable.tsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { monsterCacheClient } from "../../model/monsterCacheClient";
 import { getTeamSlots, TeamState } from "../../model/teamStateManager";
 import { AwokenSkills } from "../../model/types/monster";
@@ -13,12 +14,18 @@ export async function computeTeamUnbindablePct(
 
   var count = 0;
   var filledSlots = 0;
-  for (const slot of slots) {
+  for (const i in slots) {
+    var slot = slots[i];
     if (slot.base.id !== 0) {
       filledSlots += 1;
     }
 
     var cardBindRes = 0;
+
+    if (i === "0" && slot.base.id !== 0 && teamState[playerId].badgeId === "unbindable") {
+      cardBindRes += 1;
+    }
+
     const m1b = await monsterCacheClient.get(slot.base.id);
     if (m1b?.awakenings) {
       const unbindableRes = m1b.awakenings.filter((a) => !a.is_super && a.awoken_skill_id === AwokenSkills.UNBINDABLE);
