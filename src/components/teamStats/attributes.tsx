cbb01ae7b4e59e20386a89bebd6f55f9efd41d2e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-01-13 22:34:36 -0600

    update team attributes display

--- src/components/teamStats/attributes.tsx
+++ src/components/teamStats/attributes.tsx
@@ -1,10 +1,19 @@
 import { monsterCacheClient } from "../../model/monsterCacheClient";
 import { getTeamSlots, TeamState } from "../../model/teamStateManager";
+import { AwokenSkills } from "../../model/types/monster";
 import { GameConfig } from "../gameConfigSelector";
 
 export type AttributeHistogram = { [key: number]: boolean };
 
-export async function computeAttributes(
+export const SUBATTR_AWAKENINGS = [
+  AwokenSkills.SUBATTRRED.valueOf(),
+  AwokenSkills.SUBATTRBLUE.valueOf(),
+  AwokenSkills.SUBATTRGREEN.valueOf(),
+  AwokenSkills.SUBATTRLIGHT.valueOf(),
+  AwokenSkills.SUBATTRDARK.valueOf()
+];
+
+export async function Attributes(
   gameConfig: GameConfig,
   teamState: TeamState,
   playerId: keyof TeamState,
@@ -30,12 +39,37 @@ export async function computeAttributes(
 
   for (var s of slots) {
     const m1b = await monsterCacheClient.get(s.base.id);
+    const m1a = await monsterCacheClient.get(s.assist.id);
 
     if (m1b?.attr1 !== undefined && m1b.attr1 in attrs) {
       attrs[m1b.attr1] = true;
     }
     if (m1b?.attr2 !== undefined && m1b.attr2 in attrs) {
       attrs[m1b.attr2] = true;
+    }
+
+    var attrToAdd = undefined;
+
+    if (m1b !== undefined && m1b.awakenings !== undefined) {
+      for (const a of m1b.awakenings) {
+        const awo = a.awoken_skill.awoken_skill_id;
+        if (SUBATTR_AWAKENINGS.includes(awo)) {
+          attrToAdd = awo;
+        }
+      }
+    }
+
+    if (hasAssists && m1a !== undefined && m1a.awakenings !== undefined) {
+      for (const a of m1a.awakenings) {
+        // The equip overrides the main attr
+        const awo = a.awoken_skill.awoken_skill_id;
+        if (SUBATTR_AWAKENINGS.includes(awo)) {
+          attrToAdd = awo;
+        }
+      }
+    }
+    if (attrToAdd !== undefined) {
+      attrs[SUBATTR_AWAKENINGS.indexOf(attrToAdd)] = true;
     }
   }
 
